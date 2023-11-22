1a2f077c9060d203b8d8ed05b0afa2899b403bb2
John Zinno <johnzinno14@gmail.com>
2023-11-22 01:14:47 -0500

    update runner overhead

--- launch.sh
+++ launch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=2G
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=8G
 #SBATCH --job-name=nextflow
 #SBATCH --mail-type=FAIL,END
 #SBATCH --mail-user=jzinno@nygenome.org
